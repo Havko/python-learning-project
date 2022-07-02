759d95a9a22cddcace012220ded3e2d01d06a8f3
Steven Havko <shavko@sonicu.com>
2022-07-02 08:34:17 -0400

    Adding battepy directory

---
+++ battlepy/main.py
@@ -0,0 +1,20 @@
+class Game:
+    print("game")
+
+
+class GameBoard:
+    print("To be implemented")
+
+class Player:
+
+
+class Attack:
+
+
+
+def main():
+    print("game")
+
+if __name__ == "__main__":
+    main()
+
